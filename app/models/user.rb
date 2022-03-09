ebdb695d764dd5a6203586d7ba7f875bfb449f0b
EC2 Default User <ec2-user@ip-172-31-9-139.ec2.internal>
2022-03-09 05:46:01 +0000

    [Update]4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- has_many:user_images,dependent: :destroy
  has_many:book_path, dependent: :destroy
+ attachment :profile_image
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
-  resources :user_images, only: [:new, :create, :index, :show, :destroy]
   root 'homes#top'
   resources :books
-end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+resources :users, only: [:index, :show, :edit, :update]
+end
+ 
 
